b51cdde85a06eb7e7e831b298f0dff0bdb47d199
Matney <mlmoller21@amarillocollege.com>
2025-01-28 12:28:34 -0600

    Added code

--- Work2.1/Program.cs
+++ Work2.1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,17 @@ namespace Work2._1
     {
         static void Main(string[] args)
         {
+            //An example of the user entering a number to find the sum 
+            int number = 456;
+            int sum = 0;
+
+            while (number > 0)
+            {
+                sum += (number % 10);
+                number /= 10;
+            }
+            Console.WriteLine("The sum of digits is {0}", sum);
+
         }
     }
 }
