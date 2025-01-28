ce3d6fdd54f3bedc872441b7f34585a26f5fd683
Matney <mlmoller21@amarillocollege.com>
2025-01-28 12:36:18 -0600

    Commented

--- Work2.1/Program.cs
+++ Work2.1/Program.cs
@@ -13,13 +13,17 @@ namespace Work2._1
         {
             //An example of the user entering a number to find the sum 
             int number = 456;
+            //What the sum needs to equal out to 
             int sum = 0;
-
+            //While the number isnt equal to 0 it will continue calculations
             while (number > 0)
             {
+                
                 sum += (number % 10);
+                //Loops calculations until needed value
                 number /= 10;
             }
+            //Printing message out to user
             Console.WriteLine("The sum of digits is {0}", sum);
 
         }
