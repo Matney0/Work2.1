fbd29b24bdb5c7de476c499e57d846378716cf3c
Matney <mlmoller21@amarillocollege.com>
2025-01-28 15:05:04 -0600

    comments

--- Work2.1/Program.cs
+++ Work2.1/Program.cs
@@ -11,21 +11,31 @@ namespace Work2._1
     {
         static void Main(string[] args)
         {
-            //An example of the user entering a number to find the sum 
-            int number = 456;
-            //What the sum needs to equal out to 
-            int sum = 0;
-            //While the number isnt equal to 0 it will continue calculations
-            while (number > 0)
-            {
-                
-                sum += (number % 10);
-                //Loops calculations until needed value
-                number /= 10;
-            }
-            //Printing message out to user
-            Console.WriteLine("The sum of digits is {0}", sum);
 
+            //Prompt user to enter a number
+            Console.WriteLine("Enter number to find the sum of digits");
+            //Changes users variable into an integer 
+            var userInput = int.Parse(Console.ReadLine());
+            //Sum of the digits, starts with one before user prompt
+            var sum = 0;
+            //Does prompt until condition  met
+            do
+            {
+                //Getting final digit
+                int finalDigit = (userInput % 10);
+                
+                //Adding the digits together 
+                sum = sum + (userInput % 10);
+
+
+                //Remove final digit 
+                userInput = (userInput / 10);
+
+            }
+            while (userInput > 0);
+               
+                //Printing message out to user
+                Console.WriteLine("The sum of digits is", sum);
         }
     }
 }
